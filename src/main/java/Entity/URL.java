c40d2b85356729b76a5f63fa0f3e84fb79c2ce4a
djsiclait <djidjellypas92@gmail.com>
2016-06-19 11:36:11 -0400

    Create URL POJO Entity

--- src/main/java/Entity/URL.java
+++ src/main/java/Entity/URL.java
@@ -4,18 +4,69 @@
  */
 package Entity;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.UUID;
 
-public class URL {
+@Entity
+public class URL implements Serializable {
 
-    public String id;
-    public String originalUrl;
+    private static final long serialVersionUID = 1L;
 
+    // Attributes
+    @Id
+    @Column(name = "SHORT")
+    private String shortURL;
+
+    @Column(name = "ORIGINAL")
+    private String originalURL;
+
+    @ManyToOne  // @Column not allowe iinn many to one relationship
+    private User user;
+
+    // Constructor
     public URL() {
+
     }
 
-    public URL(String originalUrl) {
-        this.originalUrl = originalUrl;
-        this.id = UUID.randomUUID().toString().split("-")[0];
+    public URL(String originalURL, User user) {
+
+        this.setOriginalURL(originalURL);
+        this.setShortURL(UUID.randomUUID().toString().split("-")[0]);
+        this.setUser(user);
+
+    }
+
+    public URL(String shortURL, String originalURL, User user) {
+
+        this.setOriginalURL(originalURL);
+        this.setShortURL(shortURL);
+        this.setUser(user);
+
+    }
+
+    // Getters & Setters
+    public String getShortURL() {
+        return shortURL;
+    }
+
+    public void setShortURL(String shortURL) {
+        this.shortURL = shortURL;
+    }
+
+    public String getOriginalURL() {
+        return originalURL;
+    }
+
+    public void setOriginalURL(String originalURL) {
+        this.originalURL = originalURL;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
