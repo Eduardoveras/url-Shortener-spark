2af67c78b5b9314d2cb50880928401d6a1f0cc6a
Eduardo Veras <eduardo.storm@gmail.com>
2016-06-19 10:37:49 -0400

    changing git ignore

--- src/main/java/pageCreator.java
+++ src/main/java/pageCreator.java
@@ -2,4 +2,24 @@
  * Created by Eduardo veras on 19-Jun-16.
  */
 public class pageCreator {
+
+    public PageCreator() throws Exception {
+
+        //DatabaseManager.BootUP();
+        //DatabaseManager.PrintData();
+
+        generateGets();
+        generatePost();
+    }
+
+    private static void generateGets() {
+
+    }
+
+    private static void generatePost() {
+
+    }
+
+
+
 }
