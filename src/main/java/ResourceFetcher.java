8509deaad463447684c0ed8796e57ab485c76297
ejaquezcastillo <ejaquezcastillo@gmail.com>
2016-06-20 18:36:15 -0400

    realice la tarea del modulo de captura de page mediante un URL y devuelvo una variable string con dicha informacion

--- src/main/java/ResourceFetcher.java
+++ src/main/java/ResourceFetcher.java
@@ -3,15 +3,12 @@ public class ResourceFetcher {
 
     public static String getDescription(String url) {
 
-        //CHECK: http://screenshotmachine.com/apiguide.php
-
-        http://api.screenshotmachine.com/?key=[key]&size=[size]&format=[format]&hash=[hash]&cacheLimit=[cacheLimit]&timeout=[timeout]&url=[url]
-
-
-        return "Descripcion";
-    }
+            String Descripcion = "http://api.screenshotmachine.com/?key=4c2f50&size=E&format=JPG&cacheLimit=0&timeout=200&url=" + "url";
+                return "Descripcion";
+        }
 
     public static String getQrCodeURL(String url) {
+
         return "TheUrl";
     }
 
