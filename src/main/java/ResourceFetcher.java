208973b6301988b2af7b021ea18cefd711a4d8bc
ejaquezcastillo <ejaquezcastillo@gmail.com>
2016-06-20 18:51:39 -0400

    realice la tarea de la creacion del qr_code desde la URL cortada y devuelve el qr_code

--- src/main/java/ResourceFetcher.java
+++ src/main/java/ResourceFetcher.java
@@ -3,13 +3,16 @@ public class ResourceFetcher {
 
     public static String getDescription(String url) {
 
-            String Descripcion = "http://api.screenshotmachine.com/?key=4c2f50&size=E&format=JPG&cacheLimit=0&timeout=200&url=" + "url";
-                return "Descripcion";
-        }
+        String Descripcion = "http://api.screenshotmachine.com/?key=4c2f50&size=E&format=JPG&cacheLimit=0&timeout=200&url=" + url;
+
+            return "Descripcion";
+    }
 
     public static String getQrCodeURL(String url) {
 
-        return "TheUrl";
+        String TheUrl = "http://api.qrserver.com/v1/create-qr-code/?data=" + url + "&size=150x150";
+
+            return "TheUrl";
     }
 
 }
