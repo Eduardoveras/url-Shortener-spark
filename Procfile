179037bdfa9c4586c3fe10c3283c9fb6d63704ca
Eduardo Veras <eduardo.storm@gmail.com>
2016-07-01 21:45:31 -0400

    HEROKU

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:"target/dependency/*" Main
+web: ./build/install/todo/bin/app
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,22 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        port(getHerokuAssignedPort());
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
 
         staticFileLocation("/public");
-        //staticFiles.expireTime(600);
 
         DatabaseManager.BootDataBase();
 
         pageCreator pages = new pageCreator();
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+
 }
