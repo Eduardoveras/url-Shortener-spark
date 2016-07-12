a88e4afe08091df0a7ceffde6ff6494b3aa095e3
djsiclait <djidjellypas92@gmail.com>
2016-07-12 19:54:50 -0400

    Modify build.gradle and add RestCOntroller class

---
+++ src/main/java/REST/WebServiceController.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Siclait on 12/7/16.
+ */
+package REST;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceController {
+}
