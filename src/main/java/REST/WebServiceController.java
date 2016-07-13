ed360202fdf3a3b133ed251ee0cc0a566d5946d7
djsiclait <djidjellypas92@gmail.com>
2016-07-12 20:23:08 -0400

    Add Greeting Method inn WebServiceCOntroller

--- src/main/java/REST/WebServiceController.java
+++ src/main/java/REST/WebServiceController.java
@@ -3,8 +3,27 @@
  */
 package REST;
 
+import JSONTemplates.Greeting;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @RestController
 public class WebServiceController {
+
+    private static final String template = "Hello, %s! Welcome to Acorta.do";
+    private final AtomicLong counter = new AtomicLong();
+
+    // Thhis should always appear when the Client connects
+    @RequestMapping(value = "/home", method = GET)
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Guest") String name){
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
 }
