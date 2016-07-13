63f800b2a68f4e562138989f5ae9b1e28cdb7588
djsiclait <djidjellypas92@gmail.com>
2016-07-13 17:51:42 -0400

    Add NewURl, FetchOriginal  and MyURLs services

--- src/main/java/REST/WebServiceController.java
+++ src/main/java/REST/WebServiceController.java
@@ -3,7 +3,9 @@
  */
 package REST;
 
+import Entity.URL;
 import JSONTemplates.Authentication;
+import JSONTemplates.Data;
 import JSONTemplates.Greeting;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -52,7 +55,9 @@ public class WebServiceController {
         return new Authentication(counter.incrementAndGet(), WebServiceDBManager.CreateNewUser(username, firstName, lastName, password));
     }
 
-    @RequestMapping(value = "/ws/newURL/", method = GET)
+    // Access this service via following format
+    // http://localhost:8080/ws/newurl?original=XXXXXX&username=MyUsername&browser=X&os=Y&ip=MyIP&longitude=Lo&latitude=La
+    @RequestMapping(value = "/ws/newurl", method = GET)
     public Authentication NewURL(@RequestParam(name = "original") String original, @RequestParam(name = "username") String username, @RequestParam(name = "browser") String browser, @RequestParam(name = "os") String OS, @RequestParam(name = "ip") String ip, @RequestParam(name = "longitude") String longitude, @RequestParam(name = "latitude") String latitude){
 
         // TODO: Modify this service for clients
@@ -60,11 +65,39 @@ public class WebServiceController {
         return new Authentication(counter.incrementAndGet(), WebServiceDBManager.CreateNewShortURL(original, username, browser, OS, ip, longitude, latitude));
     }
 
-    @RequestMapping("/ws/fetchOriginal")
-    public void FetchOriginal(){
+    // Access this service via following format
+    // http://localhost:8080/ws/fetchoriginal?short=XXXXXX
+    @RequestMapping(value = "/ws/fetchoriginal", method = GET)
+    public Data FetchOriginal(@RequestParam(name = "short") String shortURL){
 
         System.out.println("\n\nFetching an original URL...");
+        return new Data(counter.incrementAndGet(), "OriginalURL", WebServiceDBManager.FetchOriginalURL(shortURL));
+    }
 
+    // Access this service via following format
+    // http://localhost:8080/ws/myurls?username=admin
+    @RequestMapping(value = "/ws/myurls", method = GET)
+    public Data MyURLs(@RequestParam(name = "username") String username){
+
+        System.out.println("\n\nFetching URLs for " + username + "...");
+        String buffer = "[";
+        int count = 0;
+
+        List<URL> urls = WebServiceDBManager.FetchAllURLForUser(username);
+
+        if(urls.size() == 0)
+            buffer += "]";
+
+        for (URL url:
+             urls) {
+            if(count != urls.size() - 1)
+                buffer += url.getShortURL() + ", ";
+            else
+                buffer += url.getShortURL() + "]";
+            count++;
+        }
+
+        return new Data(counter.incrementAndGet(), "ShortURL", buffer);
     }
 
 }
