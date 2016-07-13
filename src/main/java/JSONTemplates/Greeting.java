9ee0d9c399f750f2623834b0549b1a62f5b4fd1d
djsiclait <djidjellypas92@gmail.com>
2016-07-12 20:13:20 -0400

    Add Greeting class

---
+++ src/main/java/JSONTemplates/Greeting.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Siclait on 12/7/16.
+ */
+package JSONTemplates;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
