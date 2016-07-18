9797b36c074199d031b26559822a847f90086f33
djsiclait <djidjellypas92@gmail.com>
2016-07-18 18:27:11 -0400

    Add Json formater filter

--- src/main/java/JSONServiceController.java
+++ src/main/java/JSONServiceController.java
@@ -2,12 +2,15 @@
  * Created by Siclait on 18/7/16.
  */
 import static JSONTools.JSONUtil.json;
+import static spark.Spark.after;
 import static spark.Spark.get;
 
 public class JSONServiceController {
 
     public JSONServiceController() {
-        get("/json/allurls", (request, response) -> DatabaseManager.FetchAllURL(), json());
+        get("/json/allurls", (reqt, res) -> DatabaseManager.FetchAllURL(), json());
         // more routes
+
+        after("/json/*",(req, res) -> {res.type("application/json");});
     }
 }
